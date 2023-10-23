3797061059e0bfe3edef68eea9b129ccfe7cd533
CAccampo <123125295+CAccampo@users.noreply.github.com>
2023-10-23 16:15:51 -0500

    WIP: Update detection_tests.py with ArUco tests
    Began adding ArUco comparison support; untested, unfinished.

--- detection_tests.py
+++ detection_tests.py
@@ -22,13 +22,17 @@ def flip_y(y, mid):
     return (mid-y)+mid
 
 def main():
-    pyz_count, ocv_count, pyl_count = 0, 0, 0
+    pyz_count, ocv_count, pyl_count, aru_count = 0, 0, 0, 0
 
     img = cv.imread(os.path.join(test_dir_name, test_img))
 
     retval, ocv_decoded, points, _ = qcd.detectAndDecodeMulti(img)
     pyz_decoded = decode(img)
     pyl_decoded  = pyl_decode(img, shrink=3, threshold=6)
+
+    aru_dict = cv.aruco.Dictionary_get(cv2.aruco.DICT_4X4_50)
+    aru_params = cv2.aruco.DetectorParameters_create()
+    aru_points, aru_decoded, rejected_marker = cv.aruco.detectMarkers(img, aru_dict, aru_params)
 
     #blue opencv QR
     if retval:
@@ -51,8 +55,11 @@ def main():
             btm_right = [left + width, flip_y(top + height, img_mid)]
             points_pyl += [np.array([top_left, btm_left, btm_right, top_right], np.int32)]
         pyl_count = count_detects(pyl_count, img, pyl_decoded, points_pyl, (0, 0, 255))
+    #purple OpenCV ArUco marker
+    if aru_decoded:
+        aru_count = count_detects(aru_count, img, aru_decoded, np.int32(aru_points), (255,0,255))
 
-    print(ocv_count, pyz_count, pyl_count)
+    print(ocv_count, pyz_count, pyl_count, aru_count)
     img = cv.resize(img, (960, 540))  
     cv.imshow("img",img)
     cv.waitKey(0)
