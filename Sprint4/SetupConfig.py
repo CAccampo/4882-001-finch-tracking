4c9a2cc49fb623789d3247764ce6847919815d04
CAccampo <carmenaccampo@gmail.com>
2023-12-03 09:44:28 -0600

    updates config to most keys

--- Sprint4/SetupConfig.py
+++ Sprint4/SetupConfig.py
@@ -2,29 +2,51 @@ import tkinter as tk
 from tkinter.filedialog import asksaveasfile
 import json
 
-def create_window():
-    window = tk.Tk()
-    window.geometry('640x480')
-    window.title('SetupFinchConfig')
-    return window
+config_list = []
+config_path = 'config.json'
 
-def add_widgets(window):
-    head = tk.Label(window, text='Finch Configuration', font=('Times', 20))
+window = tk.Tk()
+window.geometry('480x480')
+window.title('SetupFinchConfig')
+calib_path, cb_size = tk.StringVar(), tk.StringVar()
+#Intvar appending 0s for some reason
+up_int, num_cams, code_size = tk.StringVar(),tk.StringVar(),tk.StringVar()
+
+def update_config():
+    config = load_config()
+    print(config['upload_interval'], up_int.get())
+
+    #update keys
+    config['calibration_image_paths'] = calib_path.get()
+    config['upload_interval'] = int(up_int.get())
+    config['num_cameras'] = int(num_cams.get())
+    # config['bigquery_project_id']
+    # config['bigquery_dataset_id']
+    # config['table_name']
+    # config['aruco_dictionary']
+    config['obj_real_size'] = int(code_size.get())
+    config['chessboard_size'] = [eval(i) for i in cb_size.get().split(' ')]
+    
+    save_config(config)
+
+def add_widgets():
+    head = tk.Label(window, text='Finch Configuration', font=('Times', 25))
+
     #widgets
     ##CHECK/ADD THE UNITS ON SOME OF THESE >:( )
     calib_path_label = tk.Label(window, text='Calibration:\nImage Path')
-    calib_path_entry = tk.Entry(window)
+    calib_path_entry = tk.Entry(window, textvariable=calib_path)
     cb_size_label = tk.Label(window, text='Calibration:\nChessboard Size')
-    cb_size_entry = tk.Entry(window)
+    cb_size_entry = tk.Entry(window, textvariable=cb_size)
     upload_int_label = tk.Label(window, text='Upload Interval')
-    upload_int_entry = tk.Entry(window)
+    upload_int_entry = tk.Entry(window, textvariable=up_int)
     num_cams_label = tk.Label(window, text='Number of Cameras')
-    num_cams_entry = tk.Entry(window)
+    num_cams_entry = tk.Entry(window, textvariable=num_cams)
     code_size_label = tk.Label(window, text='Barcode Size')
-    code_size_entry = tk.Entry(window)
+    code_size_entry = tk.Entry(window, textvariable=code_size)
 
 
-    sub_btn=tk.Button(window,text = 'Submit', command = '')
+    sub_btn=tk.Button(window,text = 'Submit', command = update_config)
 
     #default values
     calib_path_entry.insert(0, 'calib_img.png')
@@ -34,7 +56,7 @@ def add_widgets(window):
     code_size_entry.insert(0, 20)
 
     #placing widgets in window
-    head.grid(row=0, column=1, columnspan=3)
+    head.grid(row=0, column=0, columnspan=4)
     calib_path_label.grid(row=1,column=0)
     calib_path_entry.grid(row=1,column=1)
     cb_size_label.grid(row=1,column=2)
@@ -50,13 +72,12 @@ def add_widgets(window):
 
     window.mainloop()
 
-def load_config(file_path):
-    
-    with open(file_path, 'r') as config_file:
+def load_config():
+    with open(config_path, 'r') as config_file:
         return json.load(config_file)
 
-def save_config(file_path, config):
-    with open(file_path, 'w') as config_file:
+def save_config(config):
+    with open(config_path, 'w') as config_file:
         json.dump(config, config_file, indent=4)
         print("Configuration saved successfully.")
 
@@ -66,29 +87,9 @@ def display_config(config):
     for key, value in config.items():
         print(f"{key}: {value}")
 
-def edit_config(config):
-    print("\nEnter new values for the configuration variables (leave blank to keep current value):")
-    for key in config.keys():
-        new_value = input(f"{key} [{config[key]}]: ")
-        if new_value:
-            # Convert to appropriate type (int, list, etc.)
-            if isinstance(config[key], int):
-                config[key] = int(new_value)
-            elif isinstance(config[key], list):
-                config[key] = json.loads(new_value)
-            else:
-                config[key] = new_value
-    return config
 
 def main():
-    window = create_window()
-    add_widgets(window)
-    config_path = 'config.json'
-    config = load_config(config_path)
-
-    display_config(config)
-    config = edit_config(config)
-    save_config(config_path, config)
+    add_widgets()
 
 if __name__ == "__main__":
     main()
