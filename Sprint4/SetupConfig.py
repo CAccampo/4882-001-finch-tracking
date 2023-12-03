7abf9d659f3f893a356a136d05a7847567613c74
CAccampo <carmenaccampo@gmail.com>
2023-12-03 11:57:18 -0600

    entries for all config keys

--- Sprint4/SetupConfig.py
+++ Sprint4/SetupConfig.py
@@ -1,14 +1,14 @@
 import tkinter as tk
-from tkinter.filedialog import asksaveasfile
+from tkinter import ttk
 import json
 
 config_list = []
 config_path = 'config.json'
 
 window = tk.Tk()
-window.geometry('490x480')
+window.geometry('490x400')
 window.title('SetupFinchConfig')
-calib_path, cb_size, dict_type = tk.StringVar(), tk.StringVar(), tk.StringVar()
+calib_path, cb_size, dict_type, proj_id, data_id, table_name = tk.StringVar(), tk.StringVar(), tk.StringVar(), tk.StringVar(), tk.StringVar(), tk.StringVar()
 #Intvar appending 0s for some reason
 up_int, num_cams, code_size = tk.StringVar(),tk.StringVar(),tk.StringVar()
 
@@ -19,63 +19,74 @@ def update_config():
     config['calibration_image_paths'] = calib_path.get()
     config['upload_interval'] = int(up_int.get())
     config['num_cameras'] = int(num_cams.get())
-    # config['bigquery_project_id']
-    # config['bigquery_dataset_id']
-    # config['table_name']
+    config['bigquery_project_id'] = proj_id.get()
+    config['bigquery_dataset_id'] = data_id.get()
+    config['table_name'] = table_name.get()
     config['aruco_dictionary'] = dict_type.get()
     config['obj_real_size'] = int(code_size.get())
     config['chessboard_size'] = [eval(i) for i in cb_size.get().split(' ')]
     
     save_config(config)
 
-def add_widgets():
-    head = tk.Label(window, text='Finch Configuration', font=('Times', 25))
+def win_loop():
+    ps = 10
 
-    #widgets
+    tk.Label(window, text='Finch Tracker Configuration', font=('Times', 20)).grid(row=0, columnspan=4)
+
     ##CHECK/ADD THE UNITS ON SOME OF THESE >:( )
-    calib_path_label = tk.Label(window, text='Calibration:\nImage Path')
+    #Entries with labels
+    tk.Label(text='Calibration:').grid(sticky = 'w', row=1,pady=(ps,0))
+    tk.Label(window, text='Image Path').grid(row=2)
     calib_path_entry = tk.Entry(window, textvariable=calib_path)
-    cb_size_label = tk.Label(window, text='\nChessboard Size')
+    tk.Label(window, text='Chessboard Size').grid(row=2, column=2)
     cb_size_entry = tk.Entry(window, textvariable=cb_size)
-    upload_int_label = tk.Label(window, text='Upload Interval')
+    
+    tk.Label(text='Camera Capture:').grid(sticky = 'w', row=3, column=0,pady=(ps,0))
+    tk.Label(window, text='Upload Interval').grid(row=4)
     upload_int_entry = tk.Entry(window, textvariable=up_int)
-    num_cams_label = tk.Label(window, text='Number of Cameras')
+    tk.Label(window, text='Number of Cameras').grid(row=4, column=2)
     num_cams_entry = tk.Entry(window, textvariable=num_cams)
-    code_size_label = tk.Label(window, text='Barcode Size')
+    
+    tk.Label(text='Aruco Barcode:').grid(sticky = 'w', row=5, column=0,pady=(ps,0))
+    tk.Label(window, text='Barcode Size').grid(row=6)
     code_size_entry = tk.Entry(window, textvariable=code_size)
-    dict_type_label = tk.Label(window, text='ArUco Dictionary Type')
+    tk.Label(window, text='Dictionary Type').grid(row=6, column=2)
     dict_type_entry = tk.Entry(window, textvariable=dict_type)
+    
+    tk.Label(text='Bigquery:').grid(sticky = 'w', row=7, column=0,pady=(ps,0))
+    tk.Label(window, text='Project ID').grid(row=8, column=0)
+    proj_id_entry = tk.Entry(window, textvariable=proj_id)
+    tk.Label(window, text='Dataset ID').grid(row=8, column=2)
+    data_id_entry = tk.Entry(window, textvariable=data_id)
+    tk.Label(window, text='Table Name').grid(row=9)
+    table_entry = tk.Entry(window, textvariable=table_name)
 
+    #placing entry widgets in grid
+    calib_path_entry.grid(row=2,column=1,padx=(ps*2,0))
+    cb_size_entry.grid(row=2,column=3)
+    upload_int_entry.grid(row=4,column=1)
+    num_cams_entry.grid(row=4,column=3)
+    code_size_entry.grid(row=6,column=1)
+    dict_type_entry.grid(row=6, column = 3)
+    proj_id_entry.grid(row=8, column = 1)
+    data_id_entry.grid(row=8, column = 3)
+    table_entry.grid(row=9, column = 1)
 
-    sub_btn=tk.Button(window,text = 'Submit', command = update_config)
+    sep = ttk.Separator(window,orient='horizontal').grid(row=10, columnspan = 4,sticky='ew', pady=ps)
+    tk.Button(window,text = 'Update', command = update_config).grid(row=11,column=2, columnspan =2, pady=10)
 
+    #default values for entries
     config = load_config()
 
-    #default values
     calib_path_entry.insert(0, config['calibration_image_paths'])
     cb_size_entry.insert(0, config['chessboard_size'])
     upload_int_entry.insert(0, config['upload_interval'] )
     num_cams_entry.insert(0, config['num_cameras'] )
     code_size_entry.insert(0, config['obj_real_size'])
     dict_type_entry.insert(0, config['aruco_dictionary'])
-
-    #placing widgets in window
-    head.grid(row=0, column=0, columnspan=4)
-    
-    calib_path_label.grid(row=1,column=0)
-    calib_path_entry.grid(row=1,column=1)
-    cb_size_label.grid(row=1,column=2)
-    cb_size_entry.grid(row=1,column=3,pady=20)
-    upload_int_label.grid(row=2,column=0)
-    upload_int_entry.grid(row=2,column=1)
-    num_cams_label.grid(row=3,column=0)
-    num_cams_entry.grid(row=3,column=1)
-    code_size_label.grid(row=2,column=2)
-    code_size_entry.grid(row=2,column=3)
-    dict_type_label.grid(row=3, column = 2)
-    dict_type_entry.grid(row=3, column = 3)
-
-    sub_btn.grid(row=10,column=0, pady=20)
+    proj_id_entry.insert(0, config['bigquery_project_id'] )
+    data_id_entry.insert(0, config['bigquery_dataset_id'] )
+    table_entry.insert(0, config['table_name'])
 
     window.mainloop()
 
@@ -88,15 +99,5 @@ def save_config(config):
         json.dump(config, config_file, indent=4)
         print("Configuration saved successfully.")
 
-def display_config(config):
-    """ Display the current configuration settings. """
-    print("\nCurrent Configuration:")
-    for key, value in config.items():
-        print(f"{key}: {value}")
-
-
-def main():
-    add_widgets()
-
 if __name__ == "__main__":
-    main()
+   win_loop()
