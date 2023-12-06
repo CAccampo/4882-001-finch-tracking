0eae48b6469205a5c08a012ce49a5393184df40b
CAccampo <carmenaccampo@gmail.com>
2023-12-06 15:45:16 -0600

    in progress bird config

---
+++ Sprint4/SetupBirdConfig.py
@@ -0,0 +1,72 @@
+import tkinter as tk
+from tkinter import ttk
+import customtkinter as ck
+import json
+
+config_list = []
+config_path = 'bird_config.json'
+ck.set_appearance_mode('dark')
+
+window = ck.CTk()
+window.geometry('520x600')
+window.title('SetupBirdConfig')
+
+def add_birds(config, config_entry):
+    num_birds=int(config_entry[0].get())
+    for i in range(num_birds):
+        config[i+1] = i
+    while num_birds < len(config)-1:
+        config.popitem()
+
+    print('Changed bird number; now',num_birds)
+    save_config(config)
+def item_loop():
+    pass
+
+def update_config(config, config_entry):
+    for i, key in enumerate(config.keys()):
+        curr_config_entry = config_entry[i].get()
+        if curr_config_entry.isdigit():
+            curr_config_entry = int(curr_config_entry)
+        else:
+            try:
+                curr_config_entry = float(curr_config_entry)
+            except ValueError:
+                pass
+        config[key]=curr_config_entry
+    
+    save_config(config)
+
+def bird_win_loop():
+    ps = 10
+    config = load_config()
+    config_entry = []
+    for i in range(len(config)):
+        config_entry.append(tk.StringVar())
+
+    #Left Side Finch Config
+    ck.CTkLabel(window, text='Configure Bird IDs', font=('Times', 20)).grid(row=0, columnspan=2, padx=ps, pady=ps)
+    
+    for i, item in enumerate(config.items()):
+        key, val = item
+        ck.CTkLabel(window, text=key).grid(sticky='w',row=i+1, padx=(ps))
+        curr_entry = ck.CTkEntry(window, textvariable=config_entry[i])
+        curr_entry.grid(row=i+1, column=1, padx=(ps), pady=1)
+        curr_entry.insert(0, val)
+    ttk.Separator(window,orient='horizontal').grid(row=len(config)+1, columnspan = 2,sticky='ew', pady=ps)
+    ck.CTkButton(window,text = 'Update', command = lambda: update_config(config, config_entry)).grid(row=len(config)+2, columnspan =2, pady=10)
+    ck.CTkButton(window,text = 'Change', width=10 ,command = lambda: add_birds(config, config_entry)).grid(row=1, column=2)
+
+    window.mainloop()
+
+def load_config():
+    with open(config_path, 'r') as config_file:
+        return json.load(config_file)
+
+def save_config(config):
+    with open(config_path, 'w') as config_file:
+        json.dump(config, config_file, indent=4)
+        print("Configuration saved successfully.")
+
+if __name__ == "__main__":
+   bird_win_loop()
--- Sprint4/SetupConfig.py
+++ Sprint4/SetupConfig.py
@@ -2,29 +2,16 @@ import tkinter as tk
 from tkinter import ttk
 import customtkinter as ck
 import json
+from SetupBirdConfig import bird_win_loop, update_config, save_config, load_config
 
 config_list = []
 config_path = 'config.json'
 ck.set_appearance_mode('dark')
 
 window = ck.CTk()
-window.geometry('540x600')
-window.title('SetupFinchConfig')
+window.geometry('520x600')
+window.title('SetupConfig')
 window.resizable(False,False)
-
-def update_config(config, config_entry):
-    for i, key in enumerate(config.keys()):
-        curr_config_entry = config_entry[i].get()
-        if curr_config_entry.isdigit():
-            curr_config_entry = int(curr_config_entry)
-        else:
-            try:
-                curr_config_entry = float(curr_config_entry)
-            except ValueError:
-                pass
-        config[key]=curr_config_entry
-    
-    save_config(config)
 
 def win_loop():
     ps = 10
@@ -33,7 +20,8 @@ def win_loop():
     for i in range(len(config)):
         config_entry.append(tk.StringVar())
 
-    ck.CTkLabel(window, text='Finch Tracker Configuration', font=('Times', 20)).grid(row=0, columnspan=4, padx=ps, pady=ps)
+    #Left Side Finch Config
+    ck.CTkLabel(window, text='Finch Tracker Configuration', font=('Times', 20)).grid(row=0, columnspan=2, padx=ps, pady=ps)
     
     for i, item in enumerate(config.items()):
         key, val = item
@@ -41,19 +29,15 @@ def win_loop():
         curr_entry = ck.CTkEntry(window, textvariable=config_entry[i])
         curr_entry.grid(row=i+1, column=1, padx=(ps), pady=1)
         curr_entry.insert(0, val)
-    ttk.Separator(window,orient='horizontal').grid(row=len(config)+1, columnspan = 4,sticky='ew', pady=ps)
+    ttk.Separator(window,orient='horizontal').grid(row=len(config)+1, columnspan = 2,sticky='ew', pady=ps)
     ck.CTkButton(window,text = 'Update', command = lambda: update_config(config, config_entry)).grid(row=len(config)+2, columnspan =2, pady=10)
 
+    #Right Side ArUco Config
+    ck.CTkLabel(window, text='ArUco Barcode', font=('Times', 20)).grid(row=0, column = 2, columnspan=2)
+    ck.CTkLabel(window, text='Dictionary Type: DICT_4X4_250').grid(row=1, column = 2, columnspan=2)
+    ck.CTkButton(window,text = 'Set Bird IDs', command = bird_win_loop).grid(row=2, column=2, columnspan =2, pady=10)
+
     window.mainloop()
-
-def load_config():
-    with open(config_path, 'r') as config_file:
-        return json.load(config_file)
-
-def save_config(config):
-    with open(config_path, 'w') as config_file:
-        json.dump(config, config_file, indent=4)
-        print("Configuration saved successfully.")
 
 if __name__ == "__main__":
    win_loop()
