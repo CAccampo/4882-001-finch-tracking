c56a14a32134ddb723994c7581e5eda9a7c45266
CAccampo <carmenaccampo@gmail.com>
2023-12-10 22:48:12 -0600

    heatmap png for each cam and solved lag in animation

--- FinchTracker/PostProcessing.py
+++ FinchTracker/PostProcessing.py
@@ -1,9 +1,18 @@
 import tkinter as tk
 from tkinter import ttk
 import customtkinter as ck
+import cv2
 from heatmap import main as draw_heatmap
+from SetupBirdConfig import load_config
+import numpy as np
+import imageio.v3 as iio
 
 ps = 10
+def open_heatmap():
+    config = load_config('config.json')
+    for i in range(config['num_cameras']):
+        heatmap_img = iio.imread(f'heatmap{i}.png')
+        cv2.imshow(f'Heatmap {i}', heatmap_img)
 
 def win_loop():
     print('Initializing window')
@@ -13,7 +22,8 @@ def win_loop():
     window.resizable(False,False)
 
 
-    ck.CTkButton(window,text = 'Heatmap Animation', command = draw_heatmap, fg_color=('lightgray'),text_color=('black'), font=('Roboto',18), height=40).grid(row=3, column=2, pady=ps, padx=(ps,0))
+    ck.CTkButton(window,text = 'Heatmap Animation', command = draw_heatmap, fg_color=('lightgray'),text_color=('black'), font=('Roboto',18), height=40).grid(row=2, column=2, pady=ps, padx=(ps,0))
+    ck.CTkButton(window,text = 'Heatmap Overall', command = open_heatmap, fg_color=('lightgray'),text_color=('black'), font=('Roboto',18), height=40).grid(row=3, column=2, pady=ps, padx=(ps,0))
 
     window.mainloop()
 
--- FinchTracker/heatmap.py
+++ FinchTracker/heatmap.py
@@ -8,8 +8,10 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 from datetime import datetime, timedelta
 from SetupBirdConfig import load_config
+from IPython import display 
 
 config = load_config('config.json')
+bird_config = load_config('bird_config.json')
 PROJECT_ID = config['bigquery_project_id']
 DATASET_ID = config['bigquery_dataset_id']
 TABLE_ID = config['table_name']
@@ -18,7 +20,7 @@ client = bigquery.Client(project=PROJECT_ID)
 
 def get_data():
     query = f"""
-        SELECT data, timestamp, corner_points, distance
+        SELECT data, timestamp, corner_points, distance, camera_id
         FROM `{PROJECT_ID}.{DATASET_ID}.{TABLE_ID}`
         ORDER BY timestamp
     """
@@ -38,31 +40,75 @@ def calculate_center_point(corner_points):
 def update_heatmap(frame, data, ax):
     plotted_points = []
 
-    for i in range(frame):
-        row = data.iloc[i]
-        corner_points = np.array(row['corner_points'])
-        center_point = calculate_center_point(corner_points)
+    row = data.iloc[frame]
+    corner_points = np.array(row['corner_points'])
+    center_point = calculate_center_point(corner_points)
 
-        # Calculate alpha based on the time difference with increased scaling
-        first_timestamp = data['timestamp'].min()
-        time_difference = row['timestamp'] - first_timestamp
-        alpha_scaling_factor = 8.0  # Adjust the scaling factor for a more pronounced fading effect
-        alpha = 1.0 - (alpha_scaling_factor * time_difference / pd.Timedelta.max)
+    # Calculate alpha based on the time difference with increased scaling
+    first_timestamp = data['timestamp'].min()
+    time_difference = row['timestamp'] - first_timestamp
+    alpha_scaling_factor = 8.0  # Adjust the scaling factor for a more pronounced fading effect
+    alpha = 1.0 - (alpha_scaling_factor * time_difference / pd.Timedelta.max)
 
-        # Plot the point with fading effect
-        plotted_point = ax.plot(center_point[0], center_point[1], 'ro', alpha=alpha)[0]
-        plotted_points.append(plotted_point)
+    # Plot the point with fading effect
+    plotted_point = ax.plot(center_point[0], center_point[1], 'ro', alpha=alpha)[0]
+    plotted_points.append(plotted_point)
 
     ax.set_title(f'Accumulated Frames up to {frame}')
-    ax.set_xlabel('X-axis')
-    ax.set_ylabel('Y-axis')
 
     return plotted_points
+def heatmap_animation(start, stop):
+    data = format_data()
+    columns = ['data', 'timestamp', 'corner_points', 'distance', 'camera_id']
+    df = pd.DataFrame(data, columns=columns)
+
+    # Convert timestamp to datetime with the specified format
+    timestamp_format = "%Y-%m-%d %H:%M:%S:%f"
+    df['timestamp'] = pd.to_datetime(df['timestamp'], format=timestamp_format)
+
+    fig, ax = plt.subplots()
+    ax.set_xlabel('X-axis')
+    ax.set_ylabel('Y-axis')
+    ani = FuncAnimation(fig, update_heatmap, fargs=(df, ax), frames=len(df), repeat=False)
+    plt.show()
+
+def init_heatmap(cam_num):
+    caps = [i for i in range(config['num_cameras'])]
+    caps[cam_num] = cv2.VideoCapture(cam_num)
+    ret, frame = caps[cam_num].read()
+    #determine w/h of camera image with test frame
+
+    #create blank white image of same size
+    heatmap_img = np.ones(frame.shape, dtype = np.uint8)
+
+    return 255*heatmap_img
+    
+def draw_heatmap(center_point, data, heatmap_img, camera, dot_colors, bird_id):
+    dot_color=tuple((0,0,0))
+    if bird_id in dot_colors:
+        print('aa')
+        dot_color=dot_colors[bird_id]
+    print(bird_id,dot_color, dot_colors.keys())
+    return cv2.circle(heatmap_img, tuple(center_point.round().astype(int)), radius=0, color=dot_color, thickness=-5)
 
 
+def save_overall_heatmap():
+    data = format_data()
+    #deciding colors
+    dot_colors = {}
+    for key, value in bird_config.items():
+        dot_colors[value] = np.random.randint(0,255,size=3,)
+        dot_colors[value] = tuple(int(dot_colors[value][i]) for i in range(len(dot_colors[value])))
 
 
-def main():
+    for i in range(config['num_cameras']):
+        heatmap_img = init_heatmap(i)
+        for row in data:
+            if row[4]==i:
+                heatmap_img = draw_heatmap(np.int32(row[2]).mean(0), data, heatmap_img, i, dot_colors, int(row[0]))
+        cv2.imwrite(f'heatmap{i}.png', heatmap_img)
+
+def format_data():
     result = get_data().to_dataframe()  # Convert RowIterator to DataFrame
     data = []
 
@@ -71,18 +117,12 @@ def main():
             [int(coord) for coord in pair.replace('(', '').replace(')', '').split(",")]
             for pair in row.corner_points.split(") (")
         ]
-        data.append([row.data, row.timestamp, corner_point_list, row.distance])
+        data.append([row.data, row.timestamp, corner_point_list, row.distance, row.camera_id])
+    return data
 
-    columns = ['data', 'timestamp', 'corner_points', 'distance']
-    df = pd.DataFrame(data, columns=columns)
-
-    # Convert timestamp to datetime with the specified format
-    timestamp_format = "%Y-%m-%d %H:%M:%S:%f"
-    df['timestamp'] = pd.to_datetime(df['timestamp'], format=timestamp_format)
-
-    fig, ax = plt.subplots()
-    ani = FuncAnimation(fig, update_heatmap, fargs=(df, ax), frames=len(df), repeat=False)
-    plt.show()
+def main():
+    save_overall_heatmap()
+    heatmap_animation(10,50)
 
 if __name__ == "__main__":
     main()
